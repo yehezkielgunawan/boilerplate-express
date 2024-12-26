f12ef7d996624501ce710614f77149eba9bab1c5
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-12-26 20:30:04 +0700

    feat(myApp.js): serve index.html file on root route

--- myApp.js
+++ myApp.js
@@ -4,42 +4,8 @@ const app = express();
 console.log("Hello World!");
 
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    const absolutePath = `${__dirname}/views/index.html`;
+    res.sendFile(absolutePath);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
  module.exports = app;
