b620929f8e3014266a184f6539e1118d6cfe2085
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-12-28 11:51:11 +0700

    feat(myApp.js): add POST route for name query to return full name as JSON

--- myApp.js
+++ myApp.js
@@ -50,4 +50,9 @@ app.get("/name", (req, res) => {
 	res.json({ name: `${first} ${last}` });
 });
 
+app.post("/name", (req, res) => {
+	const { first, last } = req.body;
+	res.json({ name: `${first} ${last}` });
+});
+
 module.exports = app;
