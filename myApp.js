b2add7111f7b054056fedbd8bfc779b855f89db6
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-12-28 11:33:23 +0700

    feat(myApp.js): serve static files from the public directory

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ const app = express();
 
 console.log("Hello World!");
 
+app.use("/public", express.static(`${__dirname}/public`));
+
 app.get("/", (req, res) => {
     const absolutePath = `${__dirname}/views/index.html`;
     res.sendFile(absolutePath);
