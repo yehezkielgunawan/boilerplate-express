f025e1066f39e158fcd8fe46f843c149a6bf0c31
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-12-28 11:46:05 +0700

    feat(myApp.js): add logging middleware and current time route

--- myApp.js
+++ myApp.js
@@ -1,21 +1,41 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 
 console.log("Hello World!");
 
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.path} - ${req.ip}`);
+	next();
+});
+
+app.get(
+	"/now",
+	(req, res, next) => {
+		req.time = new Date().toString();
+		next();
+	},
+	(req, res) => {
+		res.json({ time: req.time });
+	},
+);
+
 app.use("/public", express.static(`${__dirname}/public`));
 
 app.get("/", (req, res) => {
-    const absolutePath = `${__dirname}/views/index.html`;
-    res.sendFile(absolutePath);
+	const absolutePath = `${__dirname}/views/index.html`;
+	res.sendFile(absolutePath);
 });
 
 app.get("/json", (req, res) => {
-    const message = "Hello json";
-    // make it uppercase based on environment variable
-    res.json({ message: process.env.MESSAGE_STYLE === "uppercase" ? message.toUpperCase() : message });
+	const message = "Hello json";
+	// make it uppercase based on environment variable
+	res.json({
+		message:
+			process.env.MESSAGE_STYLE === "uppercase"
+				? message.toUpperCase()
+				: message,
+	});
 });
 
-
- module.exports = app;
+module.exports = app;
